304361d62229ba62dbe7bae346ac0809203e7886
aylinaslan <98816021+aylinaslan@users.noreply.github.com>
2022-06-15 15:46:30 +0300

    Personeller girildi
    Personeller tanıtıldı bağlantılar sağlandı

---
+++ Personel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+namespace PB_Cafe
+{
+    public partial class Personel : Form
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["PBCafe"].ConnectionString;
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter da;
+        public Personel()
+        {
+            InitializeComponent();
+            doldur();
+        }
+
+        public void doldur()
+        {
+            string[] gorev = { "Şef", "Garson", "Komi" };
+            foreach (var item in gorev)
+            {
+                cb_gorev.Items.Add(item);
+            }
+        }
+
+        public void getir()
+        {
+            string sorgu = "Select * From tbl_Personel";
+            baglanti = new SqlConnection(connectionString);
+            baglanti.Open();
+            da = new SqlDataAdapter(sorgu, baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+
+        public void getir2()
+        {
+            string sorgu = "Select * From tbl_silinenPersonel";
+            baglanti = new SqlConnection(connectionString);
+            baglanti.Open();
+            da = new SqlDataAdapter(sorgu, baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView2.DataSource = tablo;
+            baglanti.Close();
+        }
+
+     
